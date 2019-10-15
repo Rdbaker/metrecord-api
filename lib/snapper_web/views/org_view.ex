a1798b180078442b4b3ccd39b2f9650323c3a721
Ryan Baker <ryan.da.baker@gmail.com>
2019-10-15 13:53:12 -0400

    Update org_view.ex

--- lib/snapper_web/views/org_view.ex
+++ lib/snapper_web/views/org_view.ex
@@ -11,4 +11,14 @@ defmodule SnapperWeb.OrgView do
   def render("org.json", %{org: org}) do
     %{id: org.id, created_at: org.inserted_at, client_id: org.client_id}
   end
+  
+  def render("private.json", %{org: org, conversations: conversations, org_properties: org_properties}) do
+    %{data: %{
+        id: org.id,
+        created_at: org.inserted_at,
+        client_id: org.client_id,
+        client_secret: org.client_secret,
+      }
+    }
+  end
 end
