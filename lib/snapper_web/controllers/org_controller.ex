7287c43ad056c298a04be96d8bb1006c0e058533
Ryan Baker <ryan.da.baker@gmail.com>
2019-10-15 13:52:27 -0400

    Update org_controller.ex

--- lib/snapper_web/controllers/org_controller.ex
+++ lib/snapper_web/controllers/org_controller.ex
@@ -2,6 +2,11 @@ defmodule SnapperWeb.OrgController do
   use SnapperWeb, :controller
 
   alias Snapper.Accounts
+  
+  def me(conn, _params) do
+    org = Accounts.get_org! conn.assigns[:current_user].org_id
+    render(conn, "private.json", %{ org: org })
+  end
 
   def show_from_client_id(conn, %{"client_id" => client_id}) do
     case Accounts.get_org_by_client_id(client_id) do
