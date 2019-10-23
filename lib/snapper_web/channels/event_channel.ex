3bb733c952ef6121248e37a4e2c938d08b4050ae
Ryan Baker <ryan.da.baker@gmail.com>
2019-10-22 22:45:31 -0400

    Updates the socket to take a different format for events

--- lib/snapper_web/channels/event_channel.ex
+++ lib/snapper_web/channels/event_channel.ex
@@ -31,31 +31,18 @@ defmodule SnapperWeb.EventChannel do
     {:noreply, socket}
   end
 
-  def handle_in("create:timer", %{"data" => data, "name" => name, "end_user_id" => end_user_id, "client_id" => client_id}, socket) do
+  def handle_in("create:track", %{"data" => data, "name" => name, "end_user_id" => end_user_id, "client_id" => client_id}, socket) do
     Events.create_event(
       client_id,
       end_user_id,
       %{
         data: data,
-        event_type: "timer",
-        name: name,
-      }
-    )
-
-    {:ok, socket}
-  end
-
-  def handle_in("create:increment", %{"data" => data, "name" => name, "end_user_id" => end_user_id, "client_id" => client_id}, socket) do
-    Events.create_event(
-      client_id,
-      end_user_id,
-      %{
-        data: data,
-        event_type: "count",
+        event_type: "track",
         name: name,
       }
     )
 
     {:noreply, socket}
   end
+
 end
